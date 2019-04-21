9a43e653a1bc3e8980382ad532115e9c011ca49b
ErikD11 <35323308+ErikD11@users.noreply.github.com>
2019-04-21 17:22:19 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace CodigoQR
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked (object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage();
+            scannerPage.Title = "Lector de QR prron";
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    DisplayAlert("Si escaneó we :v", "", "OK");
+                    codigo.Text = result.Text;
+                });
+            };
+            await Navigation.PushAsync(scannerPage);
+        }
+    }
+}
